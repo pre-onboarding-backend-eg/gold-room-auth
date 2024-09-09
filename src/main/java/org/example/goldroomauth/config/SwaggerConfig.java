2bea72b62387629a8dd6b0a6971274fb5681e8a9
jeongeungyeong <jeongeung1124@gmail.com>
2024-09-09 12:35:23 +0900

    feat : swaggerConfig 설정

---
+++ src/main/java/org/example/goldroomauth/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package org.example.goldroomauth.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder() //경로 설정
+                .group("springdoc-public")
+                .pathsToMatch("/**")
+                .build();
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("GoldRoom Auth API")
+                        .version("v1")
+                        .description("금방주식회사 인증서버 API 명세서"));
+    }
+}
